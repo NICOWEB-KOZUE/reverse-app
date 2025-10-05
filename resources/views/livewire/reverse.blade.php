b1e6d76c53e19425a2f3b2f948b6d25a2a704ad5
NICOWEB-KOZUE <mew0119@gmail.com>
2025-10-05 09:10:40 +0900

    reverse.blade.php追加

--- resources/views/livewire/reverse.blade.php
+++ resources/views/livewire/reverse.blade.php
@@ -1,11 +1,19 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, mount};
 
-//
+//state()で初期化
+state(['word','reversed']);
 
+// ページ読み込み時に{word}を受け取る
+mount(function ($word) {
+    $this->word = $word;
+    $this->reversed = strrev($word);
+});
 ?>
 
-<div>
-    //
+<div class="p-4">
+    <h1>文字列反転アプリ</h1>
+    <p>入力: {{ $word }}</p>
+    <p>反転: {{ $reversed }}</p>
 </div>
